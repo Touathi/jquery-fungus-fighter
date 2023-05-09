d457f24b30ed80a122a304356ac827e7c8f79696
Touathi Vang <Touathi@outlook.com>
2023-05-08 21:30:45 -0500

    for some reason i was not able to commit for a while, but now its working. I Created 2 variable for HP and AP, made a render function to change text for AP and HP whenever needed. Made btn functions for the attacks and subtracted the AP and HP for corrisponding attacks. Also added in if statments for when the HP or AP goes hits 0, the fungus either jumps or dead

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ $(document).ready(onReady);
 // Example:
 // let fungusHP = 100;
 
+    let currentAP = 100
+    let currentHP = 100
+
 function onReady() {
+    console.log('READY TO RUN');
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
@@ -15,4 +19,82 @@ function onReady() {
     // - Handle events that ->
     // - Updates state which is ->
     // - Rendered to the DOM
+
+    $('.arcane-scepter').on('click', arcaneScepterBtn)
+    $('.entangle').on('click', entangleBtn)
+    $('.dragon-blade').on('click', dragonBtn)
+    $('.star-fire').on('click', starFireBtn)
+
+    render();
 }
+
+function render() {
+    
+    $('.ap-text').text(currentAP)
+    console.log('Current AP: ',currentAP);
+    $('.hp-text').text(currentHP)
+    console.log('Current HP: ',currentHP);
+
+    // if (currentAP === 100) {
+    //     $('.freaky-fungus').addClass('jump')
+    // }
+    // else if ( currentAP > 0 && currentHP <= 0) {
+    //     $('.freaky-fungus').addClass('dead')
+    // }
+}
+
+function arcaneScepterBtn() {
+     currentAP -= 12
+     currentHP -= 14
+     if ( currentAP < 0 && currentHP > 0) {
+        $('.freaky-fungus').addClass('jump').removeClass('walk')
+    }       
+    else if (currentHP <= 0) {
+            $('.freaky-fungus').addClass('dead')
+        }
+    render()
+}
+
+function entangleBtn() {
+    currentAP -= 23
+    currentHP -= 9
+    
+    if ( currentAP < 0 && currentHP > 0) {
+        $('.freaky-fungus').addClass('jump').removeClass('walk')
+    }       
+    else if (currentHP <= 0) {
+            $('.freaky-fungus').addClass('dead')
+        }
+    render()
+}
+
+function dragonBtn() {
+    currentAP -= 38
+    currentHP -= 47
+    
+    if ( currentAP < 0 && currentHP > 0) {
+        $('.freaky-fungus').addClass('jump').removeClass('walk')
+    }       
+    else if (currentHP <= 0) {
+            $('.freaky-fungus').addClass('dead')
+        }
+    render()
+}
+
+function starFireBtn() {
+    currentAP -= 33
+    currentHP -= 25
+    
+    if ( currentAP < 0 && currentHP > 0) {
+        $('.freaky-fungus').addClass('jump').removeClass('walk')
+    }       
+    else if (currentHP <= 0) {
+            $('.freaky-fungus').addClass('dead')
+        }
+    render() 
+    }
+    
+
+
+
+console.log('PAGE IS LOADED!');
